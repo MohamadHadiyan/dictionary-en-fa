8efd22f1e36dc98908cf67af77258440de5751b0
Mohammad Hadiyan <hadiyanmohamad131@gmail.com>
2021-10-18 22:28:43 +0330

    j-word-1

--- js/WordStore.js
+++ js/WordStore.js
@@ -6,7 +6,7 @@ const wordStore = {
 };
 
 export const setWordStore = async () => {
-  const data = await loadData("../all-words/i-words.json");
+  const data = await loadData("../all-words/j-words.json");
   if (data) {
     wordStore.words.push(...data);
   }
